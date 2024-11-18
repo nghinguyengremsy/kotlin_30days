6faacca853cff35fd0c74359c9ed589c6e33bae2
nghi.nguyen <nghi.nguyen@inventis.vn>
2024-11-18 14:11:15 +0700

    Day 17: Builders

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,21 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
 //    for (i in 1..5) {
 //        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 //        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 //        println("i = $i")
 //    }
-    var i: Int = 5
+    val rs= getTrafficLightAction("green")
 
-    do {
-        println(i)
-        i--
-    }while (i>0)
+    println(" --> $rs")
+}
+public fun getTrafficLightAction(trafficLightState: String): String{
+    var trafficAction:String = ""
+    when(trafficLightState){
+        "Red"-> trafficAction = "Stop"
+        "Yellow"-> trafficAction ="Slow down"
+    }
+    return trafficAction
 }
